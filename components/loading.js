dea61f28af558f9c067c8a2d2630515959ed8b14
Steven Moukhametzianov <stevenmoukhametzianov@Stevens-MBP.home>
2023-03-26 13:48:20 -0400

    erer

--- components/loading.js
+++ components/loading.js
@@ -2,9 +2,5 @@ import React from "react";
 import styles from "../styles/load.module.css";
 
 export default function Loading() {
-  return (
-    <div className={styles.load}>
-      <span>Loading</span>
-    </div>
-  );
+  return <div className={styles.loader}></div>;
 }
