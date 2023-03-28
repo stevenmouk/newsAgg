3b97ba19bc73f9e2ba7447544b46ea040026a9c7
Steven Moukhametzianov <stevenmoukhametzianov@Stevens-MBP.home>
2023-03-28 00:03:34 -0400

    er

--- components/nav3.js
+++ components/nav3.js
@@ -5,13 +5,13 @@ import styles from "../styles/nav3.module.css";
 export default function Nav3() {
   return (
     <div className=" min-h-[70vh] w-full ">
-      <div className={`${styles.banner} text-2xl pt-10 pl-24 `}>Bankrun News</div>
+      <div className={`${styles.banner} text-2xl pt-5 pl-5 sm:pt-10 sm:pl-24 `}>Bankrun News</div>
       <div className=" h-[70vh] w-full flex flex-col items-center justify-center ">
-        <div className={`${styles.banner} text-[62px] text-center`}>
+        <div className={`${styles.banner} text-[32px] xs:text-[42px] sm:text-[62px] text-center `}>
           Free Finance News <br />
           Aggregator
         </div>
-        <div className={`${styles.secondary} mt-10 text-gray-600 text-xl`}>
+        <div className={`${styles.secondary} mt-10 text-gray-600 text-xl text-center px-5`}>
           Latest and best free financial news from the top websites
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "400px",
+      sm: "625px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
