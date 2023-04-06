f1aee51ad7c8ac60dd91d8ba0e1bdb0b470834ef
Steven Moukhametzianov <stevenmoukhametzianov@Stevens-MBP.home>
2023-04-05 23:54:47 -0400

    er

--- pages/api/financialtimes.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  // console.log(req.body);
-  const response = await fetch(req.body);
-
-  //https://www.ft.com/search?q=technology&sort=date
-  const html = await response.text();
-
-  res.status(200).json({ result: html });
-}
