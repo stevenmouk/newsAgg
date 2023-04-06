9d5202c916d4c53c78c156208276a36d55cede00
Steven Moukhametzianov <stevenmoukhametzianov@Stevens-MBP.home>
2023-04-06 19:06:07 -0400

    er

--- pages/api/financialtimes.js
+++ pages/api/financialtimes.js
@@ -1,7 +1,11 @@
 export default async function handler(req, res) {
-  const response = await fetch(req.body);
+  try {
+    const response = await fetch(req.body);
 
-  const html = await response.text();
+    const html = await response.text();
 
-  res.status(200).json({ result: html });
+    res.status(200).json({ result: html });
+  } catch (error) {
+    res.status(500).send("error at fetching");
+  }
 }
